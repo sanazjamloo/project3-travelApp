4830014521311adb53dc1c5cd08b57a1e2baa02c
Sanaz <sanaz.jamloo@gmail.com>
2016-10-18 10:03:25 -0700

    Create location route - working to postman
    Returning everything - need to filter by query param

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router  = express.Router();
+var Trip = require('../models/trip.js');
+var User = require('../models/user.js');
+
 
 //index
 router.get('/', function(req,res) {
@@ -7,6 +10,35 @@ router.get('/', function(req,res) {
 });
 
 
+//NOTE we are sending data to Angular.
+//We are not sending views to a user.
+
+router.get('/location' , function(req, res){
+  //first we need to figure out what location the user wants to know about.
+
+  var place = req.query.place;
+  var result = [];
+
+  //check for empty values
+  if (!place) {
+    res.json([]);
+  }
+
+  //find the relevant data in the database
+  User.find().exec()
+    .then(function(data){
+      result = data;
+    })
+    .catch(function(err){
+      console.log(err);
+    })
+    //send the data to Angular in a res.json command
+    .then(function(){
+      res.json(result);
+    });
+
+
+});
 
 
 
