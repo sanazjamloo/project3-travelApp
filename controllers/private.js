f8dcac774d1c0a6262107b0a186d202f5de91f95
Tom Murphree <tmurph1810@gmail.com>
2016-10-18 21:01:06 -0500

    Add route skeletons

--- controllers/private.js
+++ controllers/private.js
@@ -1,23 +1,24 @@
 var express = require('express');
 var router  = express.Router();
 
-//index
-router.get('/', function(req,res) {
+//logout
+router.get('/logout', function(req,res) {
   res.send('hello world');
 });
 
+//add trip
+router.post('/', function(req,res) {
+  //code goes here
+});
 
+//edit trip
+router.patch('/', function(req,res) {
+  //code goes here
+});
 
-
-
-
-
-
-
-
-
-
-
-
+//delete trip
+router.delete('/', function(req,res) {
+  //code goes here
+});
 
 module.exports = router;
