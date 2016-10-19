69b9127be5f42282cfa6a42702f4fa26b29004cc
Tom Murphree <tmurph1810@gmail.com>
2016-10-18 22:29:13 -0500

    Finish full CRUD with hard-coded userId
    Code to make it more extensible is in the file but
    isn't implemented yet.

--- controllers/private.js
+++ controllers/private.js
@@ -15,7 +15,7 @@ router.get('/logout', function(req,res) {
 //add trip
 router.post('/:userId', function(req,res) {
 
-    //   return User.findById(req.user._id).exec()
+    //User.findById(req.user._id).exec()
     User.findById('5806d92db5fd1e059a394c6e').exec()
     .then(function(user){
       // if (!user) { return; }
@@ -27,7 +27,6 @@ router.post('/:userId', function(req,res) {
       return user.save();
     })
     .then(function(data) {
-      console.log('Created and pushed new trip');
       res.status(201).json({message: 'created'});
     })
     .catch(function(err) {
@@ -37,12 +36,53 @@ router.post('/:userId', function(req,res) {
 
 //edit trip
 router.patch('/trip/:tripId', function(req,res) {
-  //code goes here
+  //User.findById(req.user._id).exec()
+  User.findById('5806d92db5fd1e059a394c6e').exec()
+  .then(function(user){
+    // if (!user) { return; }
+
+    user.trips.forEach(function(item){
+      if (item.tripId === req.params.tripId) {
+        for (property in req.body) {
+          item[property] = req.body[property];
+        }
+      }
+    });
+
+    return user.save();
+  })
+  .then(function(data) {
+    res.status(200).json({message: 'deleted'});
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 });
 
 //delete trip
 router.delete('/trip/:tripId', function(req,res) {
-  //code goes here
+  //User.findById(req.user._id).exec()
+  User.findById('5806d92db5fd1e059a394c6e').exec()
+  .then(function(user){
+    // if (!user) { return; }
+
+    var removeItemAt = user.trips.findIndex(function(item){
+      return item.tripId === req.params.tripId;
+    });
+
+    if (removeItemAt !== -1) {
+      user.trips.splice(removeItemAt, 1);
+    }
+
+    return user.save();
+  })
+  .then(function(data) {
+    console.log('removed the trip');
+    res.status(200).json({message: 'removed'});
+  })
+  .catch(function(err) {
+    console.error(err);
+  });
 });
 
 module.exports = router;
