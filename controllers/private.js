27ddcb8dbe0aaa0397b16119e415be34bd7d39f3
Tom Murphree <tmurph1810@gmail.com>
2016-10-18 22:03:22 -0500

    Get add trip to work on a hard-coded user

--- controllers/private.js
+++ controllers/private.js
@@ -1,23 +1,47 @@
 var express = require('express');
 var router  = express.Router();
+var Trip = require('../models/trip.js');
+var User = require('../models/user.js');
 
 //logout
 router.get('/logout', function(req,res) {
-  res.send('hello world');
+  try {
+    req.logout();
+  } catch (e) {
+    console.log(e);
+  }
 });
 
 //add trip
-router.post('/', function(req,res) {
-  //code goes here
+router.post('/:userId', function(req,res) {
+
+    //   return User.findById(req.user._id).exec()
+    User.findById('5806d92db5fd1e059a394c6e').exec()
+    .then(function(user){
+      // if (!user) { return; }
+
+      var newTrip = req.body;
+      newTrip.tripId = Date.now().toString();
+
+      user.trips.push(new Trip(newTrip));
+      return user.save();
+    })
+    .then(function(data) {
+      console.log('Created and pushed new trip');
+      res.status(201).json({message: 'created'});
+    })
+    .catch(function(err) {
+      console.error(err);
+    });
 });
 
 //edit trip
-router.patch('/', function(req,res) {
+router.patch('/trip/:tripId', function(req,res) {
   //code goes here
 });
 
 //delete trip
-router.delete('/', function(req,res) {
+router.delete('/trip/:tripId', function(req,res) {
   //code goes here
 });
 
